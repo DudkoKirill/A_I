4c6a2047bb63e5af5a8b60cc85ba198eca4d6609
Ilia Trubitsyn <ilia_trubitsyn@epam.com>
2018-09-30 19:50:16 +0300

    OMEGALUL

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,26 +2,28 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] start = {7,2,4,5,0,6,8,3,1};
-        int check[]={1,1,1,1,1,1,1,1,1};
-        int empty=0;
-        boolean fail=false;
-        if (start.length!=9) {
+        int[] start = {7, 2, 4, 5, 0, 6, 8, 3, 1};
+        int check[] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+        int empty = 0;
+        boolean fail = false;
+        if (start.length != 9) {
             System.out.print("Count of numbers must be 9");
             fail = true;
-        }
-        else {
-            for (int i=0;i<9 && !fail;i++)
-            {
-                if (start[i]==0)
-                    empty=i;
+        } else {
+            for (int i = 0; i < 9 && !fail; i++) {
+                if (start[i] == 0)
+                    empty = i;
                 check[start[i]]--;
-                if (check[start[i]]<0)
-                    fail=true;
+                if (check[start[i]] < 0)
+                    fail = true;
             }
         }
         if (!fail) {
@@ -31,44 +33,42 @@ public class Main {
         }
     }
 
-    static void width(State startState)
-    {
+    static void width(State startState) {
 
         System.out.println("Поиск в ширину");
+        Set<Integer> hashCheck = new TreeSet<>();
+        List<State> states = new LinkedList<>();
+        states.add(startState);
         Date start = new Date();
-        ArrayList hashCheck = new ArrayList();
-        ArrayList<State> States = new ArrayList<>();
-        States.add(startState);
-        while(States.size()>0) {
-            if(States.get(0).isFinish())
-                System.out.println("Решение найдено! Глубина: "+States.get(0).deep + " Время решения:" + ((new Date()).getTime() - start.getTime() + " мс"));
-            else if (hashCheck.indexOf(States.get(0).hash) == -1) {
-                hashCheck.add(States.get(0).hash);
-                States.get(0).setChilds();
-                for (int i = 0; i<States.get(0).childes.size(); i++)
-                    States.add(States.get(0).childes.get(i));
+        while (states.size() > 0) {
+            State state = states.get(0);
+            if (state.isFinish())
+                System.out.println("Решение найдено! Глубина: " + state.deep + " Время решения: " + ((new Date()).getTime() - start.getTime() + " мс"));
+            else if (!hashCheck.contains(state.hash)) {
+                hashCheck.add(state.hash);
+                state.setChilds();
+                states.addAll(state.childes);
             }
-            States.remove(0);
+            states.remove(0);
         }
     }
 
-    static void depth(State startState)
-    {
+    static void depth(State startState) {
         System.out.println("Поиск в глубину");
+        Set<Integer> hashCheck = new TreeSet<>();
+        List<State> states = new LinkedList<>();
+        states.add(startState);
         Date start = new Date();
-        ArrayList hashCheck = new ArrayList();
-        ArrayList<State> States = new ArrayList<>();
-        States.add(startState);
-        while(States.size()>0) {
-            if(States.get(0).isFinish())
-                System.out.println("Решение найдено! Глубина: "+States.get(0).deep + " Время решения:" + ((new Date()).getTime() - start.getTime() + " мс"));
-            else if (hashCheck.indexOf(States.get(0).hash) == -1) {
-                hashCheck.add(States.get(0).hash);
-                States.get(0).setChilds();
-                for (int i = 0; i<States.get(0).childes.size(); i++)
-                    States.add(1,States.get(0).childes.get(i));
+        while (states.size() > 0) {
+            State state = states.get(0);
+            if (state.isFinish())
+                System.out.println("Решение найдено! Глубина: " + state.deep + " Время решения: " + ((new Date()).getTime() - start.getTime() + " мс"));
+            else if (!hashCheck.contains(state.hash)) {
+                hashCheck.add(state.hash);
+                state.setChilds();
+                states.addAll(1, state.childes);
             }
-            States.remove(0);
+            states.remove(0);
         }
     }
 }
